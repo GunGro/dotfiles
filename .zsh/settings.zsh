b1b2cf262ba43cb339b5aa031c7f07758ef647c8
Gunnar Grotmol <gunnar.grotmol@gmail.com>
2021-05-19 22:37:46 +0200

    added some auto-compleation

---
+++ .zsh/settings.zsh
@@ -0,0 +1,59 @@
+# Initialize the completion
+autoload -Uz compinit && compinit -i
+zstyle ':completion:*' menu select=4
+zmodload zsh/complist
+
+# Use vim style navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+# setopt histignorealldups sharehistory
+setopt interactivecomments
+
+# good history
+HISTSIZE=1000000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+setopt appendhistory
+setopt incappendhistory
+setopt extendedhistory
+
+# Time to wait for additional characters in a sequence
+KEYTIMEOUT=1 # corresponds to 10ms
+
+# Use vim as the editor
+export EDITOR=vim
+
+# Use vim style line editing in zsh
+bindkey -v
+
+
+
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+
+
+# Set up the prompt
+
+
+autoload -Uz promptinit
+promptinit
+prompt adam1
+
