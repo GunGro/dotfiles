a70918ac9d509b4c421c828a5e680b4f89dbdbe4
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-02-11 19:13:28 +0100

    fixed setup.sh and added gitconfig. Added some text to README

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ simply run `bash path_to_repository/setup.sh`
 A prompt and aliases for zsh and plugins for completions in zsh
 Configuration and plugins for vim
 Configuration for tmux
+
+
+# Usage
+Please use the "*_local" files for local changes
--- setup.sh
+++ setup.sh
@@ -7,13 +7,17 @@ do
 	if [ -e "$HOME/.$FILE" ] || [ -L "$HOME/.$FILE" ]
 	then
 		read -p "There already exist a .$FILE file in home. Replace with symlink? ([y]/n)" -n 1 -r
+        if [[ ! -z $REPLY ]]
+        then
+            echo
+        fi
 		if [[ $REPLY =~ ^[Yy]$ ]] || [ -z $REPLY ]
 		then
 			echo "removed $HOME/.$FILE and replaced with symlink"
 			rm "$HOME/.$FILE";
 			ln -s "$SCRIPTPATH/$FILE" "$HOME/.$FILE";
 		else
-			echo "did nothing"
+			echo "Did nothing"
 		fi
 	else 
 		if ln -s "$SCRIPTPATH/$FILE" "$HOME/.$FILE";
@@ -22,3 +26,41 @@ do
 		fi
 	fi
 done
+
+#  Fix gitconfig settings if desired
+
+read -p "Do you want to include the custom gitconfig settings in your global gitconfig ([y]/n)" -n 1 -r
+if [[ ! -z $REPLY ]]
+then
+    echo
+fi
+if [[ $REPLY =~ ^[Yy]$ ]] || [ -z $REPLY ]
+then
+    echo "running 'git config --global --replace-all  include.path $SCRIPTPATH/gitconfig'"
+    git config --global --replace-all include.path "$SCRIPTPATH/gitconfig"
+    echo "running 'git config --global --add include.path $SCRIPTPATH/gitconfig_local'"
+    git config --global --add include.path "$SCRIPTPATH/gitconfig_local"
+fi
+
+# Fix YouCompleteME
+
+read -p "Do you want to set up YouCompleteME ([y]/n)" -n 1 -r
+if [[ ! -z $REPLY ]]
+then
+    echo
+fi
+if [[ $REPLY =~ ^[Yy]$ ]] || [ -z $REPLY ]
+then
+    echo "running 'sudo apt install build-essential cmake vim-nox python3-dev'"
+    sudo apt install build-essential cmake vim-nox python3-dev
+    echo "running 'sudo apt install mono-complete golang nodejs default-jdk npm'"
+    sudo apt install mono-complete golang nodejs default-jdk npm
+    cd "$SCRIPTPATH/vim/pack/vendor/start/YouCompleteMe"
+    echo "running 'python3 install.py --all'"
+    python3 install.py --all
+else
+    echo "Did nothing"
+fi
+
+
+
