7d749393c14d36449a7c057f0906c9b1e02cc111
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2026-09-06 23:42:40 +0200

    fixed thrown warning nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -46,7 +46,7 @@ return {
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "catppuccin",
+					theme = require("lualine.themes.catppuccin") or "auto",
 					component_separators = "|",
 					section_separators = "",
 				},
